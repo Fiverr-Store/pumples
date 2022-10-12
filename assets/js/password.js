e7192b63d06c24b7a0e09e0580eeb971172635a1
ali-dev-code <ali.asgr514@gmail.com>
2022-10-12 22:15:55 +0500

    updated-md

--- assets/js/password.js
+++ assets/js/password.js
@@ -73,4 +73,78 @@ $(document).ready(function () {
     }
   }); // submit form
 
+    //white list form
+    $("#white-form").submit(function (evt) {
+      var form = $(this);
+
+      evt.preventDefault();
+      evt.preventDefault();
+      $("#resultwS").hide();
+      $("#resultwE").hide();
+      const white = $("#WHITE").val();
+
+      if (white.length == "") {
+        $(".whiteError").text("Wallet is required");
+        $(".white").addClass("is-invalid");
+      } else {
+        $(".white").removeClass("is-invalid");
+      }
+
+      $(".white").focusin(function () {
+        $(".white").removeClass("is-invalid");
+      });
+      $(".white").focusout(function () {
+        if ($(this).val() === "") {
+          $(".whiteError").text(" Wallet is required");
+          $(".white").addClass("is-invalid");
+        } else {
+          $(".white").removeClass("is-invalid");
+        }
+      });
+
+
+      // check and isert email
+      if (
+        white.length != ""
+      ) {
+
+        $.ajax({
+          type: "POST",
+          url: "white.php",
+          data: { white:white },
+          dataType: "JSON",
+
+          beforeSend: function () {
+            // Before we send the request, remove the .hidden class from the spinner and default to inline-block.
+            $("#loader-ajax").removeClass("hidden");
+          },
+
+          success: function (feedback) {
+
+            if (feedback.status === "success") {
+              $("#resultwS").html(feedback.message);
+              // $('#S_Modal').modal('show');
+              $("#resultwS").show();
+              form.trigger('reset');
+
+            } else if (feedback.status === "error") {
+              $("#resultwE").html(feedback.message);
+              $("#resultwE").show();
+              form.trigger('reset');
+
+            }
+
+          },
+
+          complete: function () {
+            // Set our complete callback, adding the .hidden class and hiding the spinner.
+            $("#loader-ajax").addClass("hidden");
+          },
+
+
+        }); /// ajax call add-user
+      }
+    }); // submit form
+
+
 });
---
+++ white.php
@@ -0,0 +1,58 @@
+<?php
+// defining databas in constants
+
+defined('DB_HOST') ? null : define('DB_HOST', 'localhost');
+
+defined('DB_USER') ? null : define('DB_USER', 'root');
+
+defined('DB_PASS') ? null : define('DB_PASS', '');
+
+defined('DB_NAME') ? null : define('DB_NAME', 'mooncatz');
+
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME); // db connection
+// Check connection
+if (mysqli_connect_errno()) {
+    echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
+    exit();
+}
+// helper functions for database queries
+
+function escape($string)
+{
+    global $con;
+    return mysqli_real_escape_string($con, $string);
+}
+// execute query
+function query($query)
+{
+    global $con;
+    return mysqli_query($con, $query);
+}
+// check if error in query
+function confirm($result)
+{
+    global $con;
+    if (!$result) {
+        die('QUERY FAILED' . mysqli_error($con));
+    }
+}
+
+// row count of record
+function row_count($result)
+{
+    return  mysqli_num_rows($result);
+}
+
+
+$white = $_POST['white'];
+$white = trim($white);
+
+$sql = " SELECT * FROM wallet WHERE wallet_add = '$white' ";
+$result = query($sql);
+confirm($result);
+
+if (row_count($result) > 0) {
+    echo json_encode(['status' => 'success', 'message' => 'Congratulations! Your wallet address has been confirmed! ✔️']);
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Sorry, your wallet is not confirmed on the whitelist! ❌']);
+}
